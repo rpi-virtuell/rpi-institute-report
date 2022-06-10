1aff784ad489a01fbd22176c2239e2eb39dd68ed
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-06-10 17:21:58 +0200

    CHANGES: ADDED capabilities of custom post type ADDED taxonomy creation ADDED new role ADDED new file which can be imported into gravityforms to create reportform

--- institute-report.php
+++ institute-report.php
@@ -14,7 +14,12 @@ class InstituteReport
 {
     function __construct()
     {
+
+        //TODO: vintage query muss hinzugefügt werden damit automatisch nach dem momentanen erstellungs jahr gesucht wird
+
         add_action('init', array($this, 'register_custom_post_types'));
+        add_action('admin_init', array($this, 'add_reporter_role'));
+        add_action('init', array($this, 'register_taxonomies'));
         add_action('init', array($this, 'create_question_block'));
         add_action('gform_after_save_form', array($this, 'create_taxonomies_and_blocks'), 10, 2);
         add_action('gform_after_submission', array($this, 'create_report_section'), 10, 2);
@@ -22,6 +27,7 @@ class InstituteReport
         add_action('blocksy:single:content:bottom', array($this, 'add_editing_button_to_report_head'));
         add_action('save_post', array($this, 'update_report_sections_with_parent'), 10, 3);
     }
+
 
     public function register_custom_post_types()
     {
@@ -49,7 +55,17 @@ class InstituteReport
             "show_in_nav_menus" => true,
             "delete_with_user" => false,
             "exclude_from_search" => false,
-            "capability_type" => "post",
+            'capability_type' => array('report', 'reports'),
+            "capabilities" => array(
+                'edit_posts' => 'edit_reports',
+                'edit_others_posts' => 'edit_others_reports',
+                'read_private_posts' => 'read_private_reports',
+                'publish_posts' => 'publish_reports',
+                'read_post' => 'read_report',
+                'delete_others_posts' => 'delete_others_reports',
+                'delete_published_posts' => 'delete_published_reports',
+                'delete_posts' => 'delete_reports',
+            ),
             "map_meta_cap" => true,
             "hierarchical" => false,
             "can_export" => false,
@@ -102,6 +118,152 @@ class InstituteReport
         register_post_type("rpi_report_section", $args);
 
 
+    }
+
+    public function add_reporter_role()
+    {
+
+        add_role('reporter', 'Reporter:in');
+
+        $role = get_role('reporter');
+        $role->add_cap('read');
+        $role->add_cap('level_2');
+        $role->add_cap('level_1');
+        $role->add_cap('level_0');
+        $role->add_cap('read_report');
+        $role->add_cap('edit_reports');
+        $role->add_cap('edit_published_reports');
+        $role->add_cap('delete_reports');
+        $role->add_cap('publish_reports');
+
+
+        $roles = ['administrator', 'editor'];
+
+        foreach ($roles as $roleslug) {
+
+            $role = get_role($roleslug);
+
+
+            $role->add_cap('read_report');
+            $role->add_cap('edit_reports');
+            $role->add_cap('edit_published_reports');
+            $role->add_cap('delete_reports');
+            $role->add_cap('publish_reports');
+
+            $role->add_cap('edit_others_reports');
+            $role->add_cap('edit_published_reports');
+            $role->add_cap('delete_published_reports');
+            $role->add_cap('read_private_reports');
+            $role->add_cap('edit_private_reports');
+            $role->add_cap('delete_private_reports');
+            $role->add_cap('delete_others_reports');
+
+        }
+
+
+    }
+
+    function register_taxonomies()
+    {
+
+        /**
+         * Taxonomy: Fragen.
+         */
+
+        $labels = [
+            "name" => __("Fragen", "blocksy"),
+            "singular_name" => __("Frage", "blocksy"),
+        ];
+
+
+        $args = [
+            "label" => __("Fragen", "blocksy"),
+            "labels" => $labels,
+            "public" => true,
+            "publicly_queryable" => true,
+            "hierarchical" => true,
+            "show_ui" => true,
+            "show_in_menu" => true,
+            "show_in_nav_menus" => true,
+            "query_var" => true,
+            "rewrite" => ['slug' => 'question', 'with_front' => true,],
+            "show_admin_column" => true,
+            "show_in_rest" => true,
+            "show_tagcloud" => false,
+            "rest_base" => "question",
+            "rest_controller_class" => "WP_REST_Terms_Controller",
+            "rest_namespace" => "wp/v2",
+            "show_in_quick_edit" => true,
+            "sort" => true,
+            "show_in_graphql" => false,
+        ];
+        register_taxonomy("question", ["rpi_report_section"], $args);
+
+        /**
+         * Taxonomy: Institute.
+         */
+
+        $labels = [
+            "name" => __("Institute", "blocksy"),
+            "singular_name" => __("Institut", "blocksy"),
+        ];
+
+
+        $args = [
+            "label" => __("Institute", "blocksy"),
+            "labels" => $labels,
+            "public" => true,
+            "publicly_queryable" => true,
+            "hierarchical" => true,
+            "show_ui" => true,
+            "show_in_menu" => true,
+            "show_in_nav_menus" => true,
+            "query_var" => true,
+            "rewrite" => ['slug' => 'institute', 'with_front' => true, 'hierarchical' => true,],
+            "show_admin_column" => true,
+            "show_in_rest" => true,
+            "show_tagcloud" => false,
+            "rest_base" => "institute",
+            "rest_controller_class" => "WP_REST_Terms_Controller",
+            "rest_namespace" => "wp/v2",
+            "show_in_quick_edit" => true,
+            "sort" => false,
+            "show_in_graphql" => false,
+        ];
+        register_taxonomy("institute", ["rpi_report", "rpi_report_section"], $args);
+
+        /**
+         * Taxonomy: Jahrgänge.
+         */
+
+        $labels = [
+            "name" => __("Jahrgänge", "blocksy"),
+            "singular_name" => __("Jahrgang", "blocksy"),
+        ];
+
+
+        $args = [
+            "label" => __("Jahrgänge", "blocksy"),
+            "labels" => $labels,
+            "public" => true,
+            "publicly_queryable" => true,
+            "hierarchical" => true,
+            "show_ui" => true,
+            "show_in_menu" => true,
+            "show_in_nav_menus" => true,
+            "query_var" => true,
+            "rewrite" => ['slug' => 'vintage', 'with_front' => true, 'hierarchical' => true,],
+            "show_admin_column" => true,
+            "show_in_rest" => true,
+            "show_tagcloud" => false,
+            "rest_base" => "vintage",
+            "rest_controller_class" => "WP_REST_Terms_Controller",
+            "rest_namespace" => "wp/v2",
+            "show_in_quick_edit" => true,
+            "sort" => true,
+            "show_in_graphql" => false,
+        ];
+        register_taxonomy("vintage", ["rpi_report", "rpi_report_section"], $args);
     }
 
     public function create_question_block()
@@ -262,7 +424,7 @@ class InstituteReport
                 if ($report_block['blockName'] === 'lazyblock/report-question') {
                     if ($report_blocks[$block_key + 1]['blockName'] === null) {
                         $question = get_term_by('slug', $report_block['attrs']['term_slug'], 'question');
-                        if (is_a($question, 'WP_Term')) {
+                        if (is_a($question, 'WP_Term') && !empty(trim(strip_tags($report_blocks[$block_key + 1]['innerHTML'])))) {
                             $report_part = wp_insert_post(array(
                                 'post_author' => $post->post_author,
                                 'post_title' => $question->name . " : " . $institute->name . " : " . $vintage->name,
@@ -281,7 +443,7 @@ class InstituteReport
             }
             update_post_meta($post_ID, 'report_parts', $report_parts);
             foreach ($report_section_ids as $report_section_id) {
-                wp_delete_post($report_section_id);
+                wp_delete_post($report_section_id, true);
             }
         }
     }
